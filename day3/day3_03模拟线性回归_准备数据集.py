56433728786c77aae55147134224ee5c4c977258
21183 <2118371007@qq.com>
2026-02-12 20:27:28 +0800

    初步接触模拟线性回归

--- day3/day3_03模拟线性回归_准备数据集.py
+++ day3/day3_03模拟线性回归_准备数据集.py
@@ -20,11 +20,12 @@ plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 # 1.定义函数，创建线性回归样本数据
 def create_dataset():
     # 1.1创建数据集对象
+    # x是特征，y是真实值，coef是系数，就是反向传播过程中最后得到的权重w，如果提前知道这个w，就能很容易的算出y
     x, y, coef = make_regression(
         n_samples = 100,  # 100条样本
         n_features = 1,  # 1个特征
         noise = 10,  # 噪声，噪声越大，样本点越散，噪声越小，样本点越集中
-        coef = True,  # 是否返回系数
+        coef = True,  # 是否返回系数(反向传播最后得到的权重w)
         random_state = 6  # 随机种子
     )
     # make_regression生成的数据是nd数组，必须转换成tensor才能方便后面的操作
